ddc96e2c1404951d7efa4b01bb9477f3a2c916db
v1n1c1u <vinicius.abner1039@gmail.com>
2025-03-26 21:21:49 -0700

    style: Finishes styles following mockups

--- scripts/index.js
+++ scripts/index.js
@@ -41,16 +41,38 @@ document.addEventListener("DOMContentLoaded", () => {
       memberRole.classList.add("memberRole");
       memberRole.innerText = member.role;
 
-      const memberLinkedIn = document.createElement("a");
-      memberLinkedIn.classList.add("memberLinkedIn");
-      memberLinkedIn.href = member.linkedin;
-      memberLinkedIn.innerText = "LinkedIn";
+      const memberSocial = document.createElement("div");
+      memberSocial.classList.add("memberSocial");
+      if (member.linkedin) {
+        const memberLinkedIn = document.createElement("a");
+        memberLinkedIn.href = member.linkedin;
+        memberLinkedIn.innerHTML += `<i class="fa-brands fa-linkedin" aria-hidden="true"></i>`;
+        memberLinkedIn.innerHTML += `<span class="visually-hidden">LinkedIn</i>`;
+        memberLinkedIn.title = "LinkedIn";
+        memberSocial.append(memberLinkedIn);
+      }
+      if (member.github) {
+        const memberGitHub = document.createElement("a");
+        memberGitHub.href = member.github;
+        memberGitHub.innerHTML += `<i class="fa-brands fa-github" aria-hidden="true"></i>`;
+        memberGitHub.innerHTML += `<span class="visually-hidden">GitHub</i>`;
+        memberGitHub.title = "GitHub";
+        memberSocial.append(memberGitHub);
+      }
+      if (member.portfolio) {
+        const memberPortfolio = document.createElement("a");
+        memberPortfolio.href = member.portfolio;
+        memberPortfolio.innerHTML += `<i class="fa-brands fa-web" aria-hidden="true"></i>`;
+        memberPortfolio.innerHTML += `<span class="visually-hidden">Portfolio</i>`;
+        memberPortfolio.title = "Portfolio";
+        memberSocial.append(memberPortfolio);
+      }
 
       //Adds team member info to the member wrapper div (in order of appearance)
       gridItem.appendChild(profileImageWrapper);
       gridItem.appendChild(memberName);
       gridItem.appendChild(memberRole);
-      gridItem.appendChild(memberLinkedIn);
+      gridItem.appendChild(memberSocial);
 
       teamMemberGrid.appendChild(gridItem); //adds team member wrapper div to the grid
     });
@@ -78,7 +100,7 @@ const teamMembers = [
     picture: "../assets/images/team/vin2.png",
     github: "https://github.com/vinsouza99",
     linkedin: "https://www.linkedin.com/in/vinicius-abner",
-    role: "Lead Developer / / Full Stack Developer",
+    role: "Full Stack Developer | Lead",
   },
   ,
   {
@@ -86,7 +108,7 @@ const teamMembers = [
     picture: "../assets/images/team/cocoy2.png",
     github: "https://github.com/cocoysg",
     linkedin: "https://www.linkedin.com/in/cocoysg",
-    role: "Project Manager / / Frontend Developer",
+    role: "Frontend Developer | PM",
   },
   {
     name: "Terumasa Mori",
@@ -99,7 +121,7 @@ const teamMembers = [
     name: "Trang Nguyen",
     picture: "../assets/images/team/tara2.png",
     linkedin: "https://www.linkedin.com/in/trang-tara",
-    role: "Lead Designer / / UI/UX Designer",
+    role: "UI/UX Designer | Lead",
   },
   {
     name: "Calvin Tsai",
